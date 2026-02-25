fc07208c03832fa289960ff3150fddee883b60a9
Shamas-Tanoli <developershamas@gmail.com>
2026-02-25 20:54:25 +0500

    working with fs module

--- 03_fs_module/index.js
+++ 03_fs_module/index.js
@@ -52,5 +52,55 @@
 
 // ---- delete file  --- 
 
-const fs = require('fs/promises');
+// const fs = require('fs');
 
+// fs.unlink('./dummy.txt',(err)=>{
+//     if (err) {
+//        console.error('File delete nahi hui:', err);
+//     }
+//    console.log('File successfully deleted!');
+// })
+
+// const fs = require('fs/promises');
+
+//  const deleteFile = async ()=>{
+//     await fs.unlink('./dummy.txt');
+//      console.log('File successfully deleted!');
+// }
+// deleteFile();
+
+
+// --- delete empty directory ----- 
+// const fs = require('fs');
+
+// fs.rmdir('./shamas',(err)=>{
+//     if (err) {
+//         console.error('Folder delete nahi hua:', err);
+//         return;
+//     }
+
+//      console.log('Folder successfully deleted!');
+// })
+
+
+// --- delete empty directory with file and directory  ----- 
+// fs.rm('./shamas',{ recursive: true, force: true },(err)=>{
+//     if (err) {
+//         console.error('Folder delete nahi hua:', err);
+//         return;
+//     }
+
+//      console.log('Folder successfully deleted!');
+// })
+
+// ------------- file or folder rename -------
+
+const fs = require('fs');
+
+fs.rename('./file.txt','text.txt',(err) =>{
+ if (err){
+    console.log("error" ,err);
+    return;
+ }
+ console.log('file successfully changed!')
+})
